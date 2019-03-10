21a7d80a7f64913adf0dbceba6344ded8f1c81a1
Julie Gicquel <Julie-Gicquel_student2018@wilder.school>
2019-03-10 16:19:24 +0100

    modify containerCards

--- src/components/ListResto.js
+++ src/components/ListResto.js
@@ -14,20 +14,24 @@ export default class ListResto extends Component {
   render() {
     const change = this.state.on ? "hiddenInfos" : "showInfos";
     return (
-      <div class="cardResto">
-        <div className="headerResto">
-          <p className="nameResto">{this.props.name}</p>
-          <img className="logoResto" src={this.props.logo} alt={this.props.name}/>
-        </div>
-        <div className="showInfos">
-          <div className="containerBtn">
-              <img src={plus} alt="Cliquez" className="showBtn hoverable" onClick={()=>this.collapse()}/>
+      <div class="containerCards">
+        <div class="col-xl-8 col-lg-8 col-md-10 col-sm-12 col-12">
+          <div class="cardResto">
+            <div className="headerResto">
+              <p className="nameResto">{this.props.name}</p>
+              <img className="logoResto" src={this.props.logo} alt={this.props.name}/>
+            </div>
+            <div className="showInfos">
+              <div className="containerBtn">
+                  <img src={plus} alt="Cliquez" className="showBtn hoverable" onClick={()=>this.collapse()}/>
+              </div>
+              <div className={change} collapse={()=>this.collapse()}>
+                <p className="description">{this.props.description}</p>
+                <div className="note">Avis : {this.props.note}/5</div>
+              </div>
+              <button className="btnResto hoverable">LET'S GO !</button>
+            </div>
           </div>
-          <div className={change} collapse={()=>this.collapse()}>
-            <p className="description">{this.props.description}</p>
-            <div className="note">Avis : {this.props.note}/5</div>
-          </div>
-          <button className="btnResto hoverable">LET'S GO !</button>
         </div>
       </div>
     )
